d1b3d9a98c60fa85d1216b35280bb551e4366261
Irpan MY <irpanmy88@gmail.com>
2021-05-16 21:50:50 +0700

    first update

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    // public function index(request $request){
+    // return view('admin.index');
+    // }
+
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+       {
+            return view('admin.index');
+       }
+
+}
+ 
